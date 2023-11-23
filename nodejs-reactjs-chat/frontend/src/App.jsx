0b16cdffe2a2b2e0170f1ee06dfe91d8a2d09d70
tarang5757 <86339819+tarang5757@users.noreply.github.com>
2023-11-22 23:03:20 -0500

    added

--- nodejs-reactjs-chat/frontend/src/App.jsx
+++ nodejs-reactjs-chat/frontend/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 import "./App.css";
 
+
 import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState();
 
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
--- nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
+++ nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
@@ -1,17 +1,15 @@
-import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 const ChatsPage = (props) => {
-    
-    const chatProps = useMultiChatLogic(
-        'eee0ce40-cca3-4306-9567-309b8f181907',
-         props.user.username,
-         props.user.secret
-         );
-          
   return (
-   <div style={{height:'100vh'}}>
-    <MultiChatSocket {...chatProps} />
-    <MultiChatWindow {...chatProps} style={{height: '100%'}}/>
-  </div>
-  )
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"eee0ce40-cca3-4306-9567-309b8f181907"}
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    
+    </div>
+  );
 };
 export default ChatsPage;
